fbbce786606afcd202806bd721346030a9352ed2
nishanthan-k <krn.nishanthan@gmail.com>
2024-02-22 05:18:21 +0530

    profile section and style completed

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -22,7 +22,7 @@ const Post = ({ post }) => {
             <img src={post.profilePic} alt="" />
             <div className="details">
               <Link
-                to={`/profile:${post.userId}`}
+                to={`/profile/${post.userId}`}
                 style={{ textDecoration: "none", color: "inherit" }}
               >
                 <span className="name">{post.name}</span>
